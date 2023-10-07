f1fcfc54694590768f4619dbe17fdc5fa46ffddd
kylo74 <kyloc74@gmail.com>
2023-10-07 10:37:36 +0000

    Test calculator functions

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ list = [1,2,3,4,5,6]
 
 median = statistics.median(list)
 
-standev = statistics.stdev(np.array(list))
+standev = statistics.stdev(list)
 
 mean =  statistics.mean(list)
+
+print(median)
+print(standev)
+print(mean)
